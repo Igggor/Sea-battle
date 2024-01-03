cd869268e0705132a3e4dd73dc261ec035ba9e8d
Andrew <andrew.sidorov.10@mail.ru>
2024-01-03 13:48:32 +0300

    models init

--- sea_battle/battle/models.py
+++ sea_battle/battle/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_admin = models.BooleanField()
